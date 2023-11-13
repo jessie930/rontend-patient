3fca689a6ddfa0e6e90fbbe832c4e99bd72a7121
jessie <luhui930@126.com>
2023-11-13 14:44:42 +0100

    #3 Implement user homepage UI

---
+++ client/src/components/Homepage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="home" class="container col-xxl-10 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5 mb-5">
+            <div class="col-lg-6 col-sm-8 col-lg-6">
+                <img src="@/assets/23.png" class="d-block mx-lg-auto img-fluid" alt="Dental" >
+            </div>
+            <div class="col-6">
+                <h4 class="display-6 fw-bold text-body-emphasis lh-1 mb-3">Welcome to Gothenburg Dentist Clinic !</h4>
+                <p> </p>
+                <br>
+                <p class="lead">Your Partner in Dental Health and Wellness, 
+                  we're more than just a dental clinic; we're a haven for your smile and oral health. 
+                  Nestled in the heart of [Location], our clinic combines state-of-the-art technology with a warm, 
+                  welcoming environment, ensuring every visit is comfortable, effective, 
+                  and tailored to your unique dental needs.</p>
+               
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  
+    
+    <script>
+    export default {
+      name: 'DentalClinic',
+      methods: {
+        searchAppointments() {
+          
+          alert('。');
+        }
+      }
+    }
+    </script>
+    
+    <style>
+   
+    </style>
+    
+  
+  
