196de3683441c413885102d32aaea7e339141368
jessie <luhui930@126.com>
2023-11-13 22:28:07 +0100

    #8 add register router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Booking from '../components/Booking.vue'
 import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
 
 
 
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     },
     
     
