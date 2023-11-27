1a987d19b495e0690e5d1b76c79ddcd374487307
jessie <luhui930@126.com>
2023-11-27 22:57:08 +0100

    #8 fix merge conflict

--- client/src/axios.js
+++ client/src/axios.js
@@ -14,16 +14,13 @@ mock.onDelete(new RegExp('/iterms/\\d+')).reply(200, {
 });
 
 mock.onPost('/submit-form').reply(200, {
-<<<<<<< Updated upstream:client/axios.js
-  message: 'Form submitted successfully'
-=======
   message: 'Booking Form submitted successfully'
 });
 
 mock.onPost('/api/login').reply(200, {
   token: 'mocked-token' ,// assum token
   message: 'Mock login successfully'
->>>>>>> Stashed changes:client/src/axios.js
+
 });
 
 mock.onPost('/api/register').reply(200, {
