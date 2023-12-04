ce8acf78a3512f6d82ec67a20633dc18a0a00ef8
jessie <luhui930@126.com>
2023-12-04 22:43:11 +0100

    #8 Implement registration axios request

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -84,28 +84,24 @@
 
   try {
     const response = await axios.post('http://localhost:8000/api/v1/patients/', userData);
+    alert(`Email: ${email} Registration successful!`);
     console.log('Registration successful:', response.data);
     this.redirectToLogin();
   } catch (error) {
     if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
+      
       console.log(error.response.data);
       console.log(error.response.status);
       console.log(error.response.headers);
-      // Here you can add a user-friendly error message or action
+      
     } else if (error.request) {
-      // The request was made but no response was received
       console.log(error.request);
     } else {
-      // Something happened in setting up the request that triggered an Error
       console.log('Error', error.message);
     }
   }
 }
   },
-}
-
-  
+} 
 </script>
   
