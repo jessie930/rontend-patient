29674ad350135a07a2d52f9c0b78167a910e48e5
Nasit Vurgun <nasit@student.chalmers.se>
2024-01-08 22:39:33 +0100

    Implement websocket for counting users

--- client/src/main.js
+++ client/src/main.js
@@ -3,15 +3,23 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-//import store from './store';
-
-
-
-
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+
+const API_GATEWAY = import.meta.env.VITE_API_GATEWAY;
+
+
+console.log("API_GATEWAY: ", API_GATEWAY);
+
+// Connect to the WebSocket server
+const socket = io(`ws://${API_GATEWAY}`);
+
+// Event handler for connection
+socket.on('connect', () => {
+    console.log('Connected to WebSocket server');
+});
 
 const app = createApp(App)
 
